bd8290350207af5128f7ecc34e08a3dc05e5eeb7
TeeMak <bunth667@gmail.com>
2019-05-20 12:52:12 -0400

    Working on chat portal

---
+++ chat.js
@@ -0,0 +1,29 @@
+//CLIENT SIDE
+console.log("CHAT.js connected");
+const socket = io.connect("http://localhost:5000");
+
+const message = document.getElementById("message");
+const handle = document.getElementById("handle");
+const btn = document.getElementById("send");
+const output = document.getElementById("output");
+const feedback = document.getElementById("feedback");
+
+btn.addEventListener("click", () => {
+	//sending message to server
+	socket.emit("chat", { message: message.value, handle: handle.value });
+});
+
+message.addEventListener("keypress", () => {
+	socket.emit("typing", handle.value);
+});
+//listening for events
+socket.on("chat", data => {
+	//clears input field
+	feedback.innerHTML = "";
+	output.innerHTML += `<p><strong>${data.handle}: ${data.message}</strong></p>`;
+});
+
+socket.on("typing", data => {
+	//
+	feedback.innerHTML = `<p><em>${data} is typing... </em></p>`;
+});
