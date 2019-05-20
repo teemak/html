14402ee97dfd413384fb026b87fa8e0d18d8dc29
TeeMak <bunth667@gmail.com>
2019-05-20 13:59:17 -0400

    added port 7777 to socket server

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 //CLIENT SIDE
-const socket = io.connect("https://teematrix.vinixtech.net");
+const socket = io.connect("https://teematrix.vinixtech.net:7777");
 console.log("CLIENT SOCKET\n", socket);
 //const socket = io.connect("http://localhost:5000");
 
